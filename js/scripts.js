44d4f699e60d3956326f815c0591cbab689dfe3d
Dennelle <dennelle.walton@gmail.com>
2023-10-02 15:56:31 -0400

    setup array iterator methods lab

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Array Iterator Methods
+
+//imperative is step by step, such as a for loop.
+
+//functional or declarative is condensed. It has a value and an index that runs through all the steps as a for loop.
+
+/* forEach, map, filter, find, findIndex, some, every, reduce*/
+
+const friends = ['Melissa', 'Marc', 'Andrew', 'Nick']
+
+friends.forEach((friend) =>{
+    console.log(`I have a friend named $(friend)`)
+
+})
